11afb7bf840592e72acfc3064f18db792b1147f4
Kotesh <jkoteswararao@yahoo.com>
2020-08-25 14:05:50 +0530

    comment change at the end

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#thisis a achange that I am going to make to the code to make it universable access
